57cbfd12041de8dbed2db83a265a3005e9c51640
Evanzap <49214424+Evanzap@users.noreply.github.com>
2022-08-19 17:04:04 -0600

    attempt at implementing sensors.

--- main.lua
+++ main.lua
@@ -25,9 +25,10 @@ function love.load()
     --jumping radius
     --width radius = 9
     --height radius = 14
+    debug=true
     p.pushradius=0
     p.slopefactor=0
-    p.multiplyer=0.25
+    p.multiplyer=1
     p.jumpforce=6.5*p.multiplyer
     p.accelerationspeed= 0.046875*p.multiplyer
     p.decelerationspeed= 0.5*p.multiplyer
@@ -35,6 +36,7 @@ function love.load()
     p.topspeed= 6*p.multiplyer
     p.sensoraxpos=0
     p.sensorbxpos=0
+    p.yspeed=0
 end
 
 function dump(o)
@@ -51,6 +53,9 @@ function dump(o)
  end
 
 function love.update(dt)
+    if dt < 1/30 then
+        love.timer.sleep(1/30 - dt)
+     end
     local up    = love.keyboard.isScancodeDown('up')
 	local down  = love.keyboard.isScancodeDown('down')
 	local left  = love.keyboard.isScancodeDown('left')
@@ -85,14 +90,13 @@ function love.update(dt)
             end
         end
     end
-    p.x=p.x+p.groundspeed
     else
         if right then p.x=p.x+0.5 end
         if left then p.x=p.x-1 end
-        if down then p.y=p.y+1 end
+        if down then p.y=p.y+0.5 end
         if up then p.y=p.y-1 end
     end
-
+    --Sensor Information Hotkeys
     if b then
         local tile1, tile2 = testlevel:convertPixelToTile(p.x-p.widthradius, p.y+p.heightradius)
         local tileprops = testlevel:getTileProperties(1,  math.floor(tile1+1), math.floor(tile2+1))
@@ -112,6 +116,7 @@ function love.update(dt)
     if n then
         local tile1, tile2 = testlevel:convertPixelToTile(p.x+p.widthradius, p.y+p.heightradius)
         local tileprops = testlevel:getTileProperties(1,  math.floor(tile1+1), math.floor(tile2+1))
+        local tilepropsregression = testlevel:getTileProperties(1,  math.floor(tile1+1), math.floor(tile2))
         local tilexpos = math.floor(p.x+p.widthradius)-math.floor(tile1)*16
         local arraynumber = tostring(tilexpos)
         print("Sensor B Info.")
@@ -120,12 +125,38 @@ function love.update(dt)
         print("Test Thing: ".. math.floor((p.y+p.heightradius)-math.floor(tile2)*16))
         if tileprops[arraynumber] == nil then
             print("Tile Height: nil")
+            print("Distance From Surface: nil")
         else
         print("Tile Height: ".. tileprops[arraynumber])
+        if tileprops["full"]==true and tilepropsregression["solid"] == true then
+            print("Distance From Surface: ".. (math.floor((p.y+p.heightradius)+tilepropsregression[arraynumber]-math.floor(tile2)*16)))
+        else
+            print("Distance From Surface: ".. (tileprops[arraynumber]+math.floor((p.y+p.heightradius)-math.floor(tile2+1)*16)))
+        end
         end
         print("X (in tiles): ".. math.floor(tile1))
         print("Y (in tiles): ".. math.floor(tile2))
     end
+    --Attempt at implementation stuff
+    debug=false
+    local tile1, tile2 = testlevel:convertPixelToTile(p.x+p.widthradius, p.y+p.heightradius)
+    local tileprops = testlevel:getTileProperties(1,  math.floor(tile1+1), math.floor(tile2+1))
+    local tilepropsregression = testlevel:getTileProperties(1,  math.floor(tile1+1), math.floor(tile2))
+    local tilepropsextension = testlevel:getTileProperties(1,  math.floor(tile1+1), math.floor(tile2-1))
+    local tilexpos = math.floor(p.x+p.widthradius)-math.floor(tile1)*16
+    local arraynumber = tostring(tilexpos)
+    p.yspeed=0
+    if tileprops["full"] and tilepropsregression["solid"] == true and tilepropsregression[arraynumber]>=0 then
+            print("Distance From Surface: ".. (math.floor((p.y+p.heightradius)+tilepropsregression[arraynumber]-math.floor(tile2)*16)))
+            p.yspeed=math.floor(p.y+p.heightradius)+tilepropsregression[arraynumber]-math.floor(tile2)*16*-1
+        elseif tileprops["solid"] ==false  and tilepropsextension[arraynumber]>0 then
+            p.yspeed=(tileprops[arraynumber]+math.floor(p.y+p.heightradius)-math.floor(tile2+2)*16)
+        elseif tileprops["solid"] then
+        print("Distance From Surface: ".. (tileprops[arraynumber]+math.floor((p.y+p.heightradius)-math.floor(tile2+1)*16)))
+        p.yspeed=(tileprops[arraynumber]+math.floor(p.y+p.heightradius)-math.floor(tile2+1)*16)*-1-1
+    end
+    p.x=p.x+p.groundspeed
+    p.y=p.y+p.yspeed
 end
 
 function love.draw()
