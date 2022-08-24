42a0aa4d12471e955fff4378ee55be36730e988f
Evanzap <49214424+Evanzap@users.noreply.github.com>
2022-08-23 18:47:16 -0600

    Added sonic sprite and added slope factor

--- main.lua
+++ main.lua
@@ -11,7 +11,7 @@ function love.load()
     local sti = require "sti"
     testlevel = sti("maps/testlevel.lua")
     love.graphics.setDefaultFilter( 'nearest', 'nearest' )
-    
+    sonic = love.graphics.newImage("somic.png")
     p = {}
     p.x=10*12+0.5
     p.y=12*16+0.5-4
@@ -25,7 +25,7 @@ function love.load()
     --jumping radius
     --width radius = 9
     --height radius = 14
-    debug=true
+    debug=false
     p.pushradius=0
     p.slopefactor=0
     p.multiplyer=1
@@ -36,7 +36,9 @@ function love.load()
     p.topspeed= 6*p.multiplyer
     p.sensoraxpos=0
     p.sensorbxpos=0
-    p.yspeed=0
+    p.slopefactor=0.125*p.multiplyer
+    p.slopefactorup=0.78125*p.multiplyer
+    p.slopefactordown=0.3125*p.multiplyer
 end
 
 
@@ -67,44 +69,6 @@ function love.update(dt)
 	local right = love.keyboard.isScancodeDown('right')
     local b = love.keyboard.isScancodeDown('b')
     local n = love.keyboard.isScancodeDown('n')
-    if not debug then
-    if left then
-        if p.groundspeed>0 then --if moving to the right
-            p.groundspeed = p.groundspeed-p.decelerationspeed  --decelerate
-            if p.groundspeed <= 0 then
-                p.groundspeed = -0.5  --emulate deceleration quirk
-            end
-        elseif p.groundspeed > -p.topspeed then --if moving to the left
-            p.groundspeed =  p.groundspeed- p.accelerationspeed  --accelerate
-            if p.groundspeed <= -p.topspeed then
-                p.groundspeed = -p.topspeed --impose top speed limit
-            end
-        end
-    end
-
-    if right then
-        if p.groundspeed<0 then --if moving to the left
-            p.groundspeed = p.groundspeed+p.decelerationspeed  --decelerate
-            if p.groundspeed >= 0 then
-                p.groundspeed = 0.5  --emulate deceleration quirk
-            end
-        elseif p.groundspeed < p.topspeed then --if moving to the right
-            p.groundspeed =  p.groundspeed+ p.accelerationspeed  --accelerate
-            if p.groundspeed >= p.topspeed then
-                p.groundspeed = p.topspeed --impose top speed limit
-            end
-        end
-    end
-
-    if not left and not right then
-        p.groundspeed = p.groundspeed-math.min(math.abs(p.groundspeed), p.frictionspeed) * sign(p.groundspeed)
-    end
-    else
-        if right then p.x=p.x+0.5 end
-        if left then p.x=p.x-1 end
-        if down then p.y=p.y+0.5 end
-        if up then p.y=p.y-1 end
-    end
     --Sensor Information Hotkeys
     if b then
         local tile1, tile2 = testlevel:convertPixelToTile(p.x-p.widthradius, p.y+p.heightradius)
@@ -146,28 +110,73 @@ function love.update(dt)
         print("X (in tiles): ".. math.floor(tile1))
         print("Y (in tiles): ".. math.floor(tile2))
     end
-    --Attempt at implementation stuff
+            if not debug then
+                p.groundspeed = p.groundspeed - (p.slopefactor * math.sin(p.groundangle*0.0174533))
+
+                if left then
+                    if p.groundspeed>0 then --if moving to the right
+                        p.groundspeed = p.groundspeed-p.decelerationspeed  --decelerate
+                        if p.groundspeed <= 0 then
+                            p.groundspeed = -0.5  --emulate deceleration quirk
+                        end
+                    elseif p.groundspeed > -p.topspeed then --if moving to the left
+                        p.groundspeed =  p.groundspeed- p.accelerationspeed  --accelerate
+                        if p.groundspeed <= -p.topspeed then
+                            p.groundspeed = -p.topspeed --impose top speed limit
+                        end
+                    end
+                end
+            
+                if right then
+                    if p.groundspeed<0 then --if moving to the left
+                        p.groundspeed = p.groundspeed+p.decelerationspeed  --decelerate
+                        if p.groundspeed >= 0 then
+                            p.groundspeed = 0.5  --emulate deceleration quirk
+                        end
+                    elseif p.groundspeed < p.topspeed then --if moving to the right
+                        p.groundspeed =  p.groundspeed+ p.accelerationspeed  --accelerate
+                        if p.groundspeed >= p.topspeed then
+                            p.groundspeed = p.topspeed --impose top speed limit
+                        end
+                    end
+                end
+                    if p.groundangle==0 then
+                    p.xspeed= p.groundspeed * math.cos(p.groundangle*0.0174533)
+                    p.yspeed= p.groundspeed * -math.sin(p.groundangle*0.0174533)
+                    else
+                        p.xspeed = p.groundspeed * math.cos(p.groundangle*0.0174533)
+                        p.yspeed = p.groundspeed * -math.sin(p.groundangle*0.0174533)
+                    end
+        
+            if not left and not right then
+                p.groundspeed = p.groundspeed-math.min(math.abs(p.groundspeed), p.frictionspeed) * sign(p.groundspeed)
+            end
+
+                --Attempt at implementation stuff
+        p.x = p.x + p.xspeed
+        p.y = p.y + p.yspeed
     --SENSOR B
-    debug=false
     local tile1, tile2 = testlevel:convertPixelToTile(p.x+p.widthradius, p.y+p.heightradius)
     local tileprops = testlevel:getTileProperties(1,  math.floor(tile1+1), math.floor(tile2+1))
     local tilepropsregression = testlevel:getTileProperties(1,  math.floor(tile1+1), math.floor(tile2))
     local tilepropsextension = testlevel:getTileProperties(1,  math.floor(tile1+1), math.floor(tile2+2))
     local tilexpos = math.floor(p.x+p.widthradius)-math.floor(tile1)*16
     local arraynumber = tostring(tilexpos)
-    p.x=p.x+p.groundspeed
     sensorblength=0
         if tileprops["solid"] then
             print("Distance From Surface: ".. (tileprops[arraynumber]+math.floor((p.y+p.heightradius)-math.floor(tile2+1)*16)))
             sensorblength=(tileprops[arraynumber]+math.floor(p.y+p.heightradius)-math.floor(tile2+1)*16)*-1-1
+            sensorbgroundangle=tileprops["angle"]
         elseif tileprops[arraynumber] == 0 and tilepropsextension[arraynumber] >= 0 then
                 print("B Extension")
                 sensorblength=math.floor((math.floor(p.y+p.heightradius)-math.floor(tile2)*16)-16)*-1-1+16-tilepropsextension[arraynumber]
+                sensorbgroundangle=tilepropsextension["angle"]
                 print(sensorblength)
         elseif tileprops["full"] and tilepropsregression["solid"] == true and tilepropsregression[arraynumber]>=0 then
             print("Regression")
             print("Distance From Surface: ".. (math.floor(p.y+p.heightradius)+tilepropsregression[arraynumber]+math.floor(tile2)*16*-1))
             sensorblength=(math.floor(p.y+p.heightradius)+tilepropsregression[arraynumber]+math.floor(tile2)*16)*-1
+            sensorbgroundangle=tilepropsregression["angle"]
 
         end
         --SENSOR A
@@ -177,36 +186,49 @@ function love.update(dt)
         local tilepropsextension = testlevel:getTileProperties(1,  math.floor(tile1+1), math.floor(tile2+2))
         local tilexpos = math.floor(p.x-p.widthradius)-math.floor(tile1)*16
         local arraynumber = tostring(tilexpos)
-        p.x=p.x+p.groundspeed
         sensoralength=0
             if tileprops["solid"] then
                 print("Distance From Surface: ".. (tileprops[arraynumber]+math.floor((p.y+p.heightradius)-math.floor(tile2+1)*16)))
                 sensoralength=(tileprops[arraynumber]+math.floor(p.y+p.heightradius)-math.floor(tile2+1)*16)*-1-1
+                sensoragroundangle=tileprops["angle"]
             elseif tileprops[arraynumber] == 0 and tilepropsextension[arraynumber] >= 0 then
                     print("A Extension")
                     sensoralength=math.floor((math.floor(p.y+p.heightradius)-math.floor(tile2)*16)-16)*-1-1+16-tilepropsextension[arraynumber]
+                    sensoragroundangle=tilepropsextension["angle"]
+                    print(p.groundangle)
                     print(sensoralength)
             elseif tileprops["full"] and tilepropsregression["solid"] == true and tilepropsregression[arraynumber]>=0 then
                 print("Regression")
                 print("Distance From Surface: ".. (math.floor(p.y+p.heightradius)+tilepropsregression[arraynumber]+math.floor(tile2)*16*-1))
                 sensoralength=(math.floor(p.y+p.heightradius)+tilepropsregression[arraynumber]+math.floor(tile2)*16)*-1
+                sensoragroundangle=tilepropsregression["angle"]
             end
 
             if sensoralength<sensorblength then
                 p.y=p.y+sensoralength
+                p.groundangle=sensoragroundangle
                 print("A WON")
             elseif sensorblength<sensoralength then
                 p.y=p.y+sensorblength
+                p.groundangle=sensorbgroundangle
                 print("B WON")
             elseif sensoralength==sensorblength then
                 p.y=p.y+sensoralength
+                p.groundangle=0
                 print("BOTH WERE EQUAL")
             end
-    p.y=p.y+p.yspeed
+
+            else
+                if right then p.x=p.x+0.5 end
+                if left then p.x=p.x-1 end
+                if down then p.y=p.y+0.5 end
+                if up then p.y=p.y-1 end
+            end
 end
 
 function love.draw()
     love.graphics.scale(2,2)
+    love.graphics.draw(sonic, p.x-p.widthradius-7,p.y-p.heightradius)
     love.graphics.setColor(1, 1, 1)
     testlevel:draw(0,0,2,2)
     love.graphics.setColor(0, 1, 0)
